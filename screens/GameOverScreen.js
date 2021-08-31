403d1d990bb961c3a8c6a838050ed0f577513245
unknown <erik@computer.com>
2021-08-30 21:42:44 -0400

    all game logic written

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,10 +1,15 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, Button} from 'react-native';
+import Card from'../components/Card'
 
 const GameOver = props => {
     return (
         <View style={styles.screen}>
             <Text>Game Over!</Text>
+            <Text>Number of Rounds:</Text>
+            <Card><Text>{props.roundsNumber}</Text></Card>
+            <Text>Number was: {props.userNumber}</Text>
+            <Button title="New Game" onPress={props.onRestart} />
         </View>
     );
 }
